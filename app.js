973e1c7932748dcb11904ad9979fbc26a5074001
Matthew Pegula <pegula@gmail.com>
2011-12-05 10:35:31 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,234 @@
+var express = require('express');
+var app = express.createServer();
+var io = require('socket.io').listen(app);
+
+io.configure(function(){
+  io.set('heartbeat timeout', 3);
+  io.set('heartbeat interval', 2);						
+});
+
+MemoryStore = express.session.MemoryStore;
+sessionStore = new MemoryStore();
+
+
+// = Config =
+
+app.configure(function () {
+    app.use(express.cookieParser());
+    app.use(express.session({store: sessionStore , secret: 'secret' , key: 'express.sid'}));
+
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(app.router);
+	app.set('view engine', 'ejs');
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+io.set('log level', 1); // less verbose
+
+// = Helpers =
+
+// = Routes =
+
+app.get('/', function (req, res) {
+	
+	var code = "";
+	var possible = "123456789";
+
+    for( var i=0; i < 3; i++ ) {
+        code += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	
+	res.render('index', {code: code});
+});
+
+
+
+app.get('/lights/:strand_id', function (req, res) {
+	
+	var degrees = [];
+	for(var i=0; i<20; i++) {
+		degrees[i] = Math.floor(Math.random() * 20);
+		
+		if (degrees[i] % 2 == 0) {
+			degrees[i] = degrees[i] * -1;
+		}		
+		
+		if (degrees[i] % 7 == 0) {
+			degrees[i] = degrees[i] + 180;
+		}		
+	}
+	
+	var colors = [];
+	var color_defs = new Array("real", "real", "real", "real", "real", "real");
+	for(var i=0; i<20; i++) {
+		var x = (Math.floor(Math.random() * 6));
+		colors[i] = color_defs[x];
+	}	
+
+	var offsets = [];
+	for(var i=0; i<20; i++) {
+		var x = (Math.floor(Math.random() * 20));
+		offsets[i] = -1 * x;
+		if (degrees[i] >= 160) {
+			offsets[i] = offsets[i] - 90;
+		}
+	}	
+	
+	req.session.strand_id = req.params.strand_id;
+	res.render('lights', {layout: 'lights_layout.ejs', 'degrees' :degrees, 'colors' : colors, 'offsets': offsets, 'is_new' : 0, 'strand_id' : req.params.strand_id});	
+});
+
+app.get('/lights/new/:strand_id', function (req, res) {
+	
+	var degrees = [];
+	for(var i=0; i<20; i++) {
+		degrees[i] = Math.floor(Math.random() * 20);
+		if (degrees[i] % 2 == 0) {
+			degrees[i] = degrees[i] * -1;
+		}		
+	}
+	
+	var colors = [];
+	var color_defs = new Array("real", "real", "real", "real", "real", "real");
+	for(var i=0; i<20; i++) {
+		var x = (Math.floor(Math.random() * 6));
+		colors[i] = color_defs[x];
+	}	
+
+	var offsets = [];
+	for(var i=0; i<20; i++) {
+		var x = (Math.floor(Math.random() * 20));
+		offsets[i] = -1 * x;
+	}	
+	
+	req.session.strand_id = req.params.strand_id;
+	res.render('lights', {layout: 'lights_layout.ejs', 'degrees' :degrees, 'colors' : colors, 'offsets': offsets, 'strand_id' : req.params.strand_id, 'is_new' : 1});	
+});
+
+
+// Start server
+app.listen(8000);
+
+
+var Session = require('connect').middleware.session.Session;
+var parseCookie = require('connect').utils.parseCookie;
+io.set('authorization', function (data, accept) {
+    // check if there's a cookie header
+    if (data.headers.cookie) {
+        // if there is, parse the cookie
+        data.cookie = parseCookie(data.headers.cookie);
+        // note that you will need to use the same key to grad the
+        // session id, as you specified in the Express setup.
+        data.sessionID = data.cookie['express.sid'];
+    } else {
+       // if there isn't, turn down the connection with a message
+       // and leave the function.
+       return accept('No cookie transmitted.', false);
+    }
+    // accept the incoming connection
+    accept(null, true);
+});
+
+// Socket stuff
+var connected = [];
+var playing = [];
+var playing_started = [];
+
+var lights = [];
+
+io.sockets.on('connection', function (socket) {
+	
+	var id = socket.handshake.sessionID;
+	var parts = socket.handshake.headers.referer.split('/');
+	var strand_id = parts[parts.length -1];
+	
+	if (connected[strand_id]) {
+		connected[strand_id] = connected[strand_id] + 1;
+	} else {
+		connected[strand_id] = 0;
+	}
+	
+	if (!lights[strand_id])  {
+		lights[strand_id] = [];
+	}
+	
+	if (!playing[strand_id])  {
+		playing[strand_id] = [];
+	}
+	
+	if (!playing_started[strand_id])  {
+		playing_started[strand_id] = [];
+	}
+				
+	lights[strand_id].push(id);	
+	
+	console.log('Client connected! ('+ lights[strand_id].length +') ('+ id +') + ('+ strand_id +')' );
+	socket.emit('set_id', id);
+	socket.emit('set_strand_id', strand_id);
+	
+	io.sockets.emit('alert_count', {'strand_id': strand_id, 'count': lights[strand_id].length});	
+	
+	if (lights[strand_id].length == 1) {
+		playing[strand_id] = 0;
+		io.sockets.emit('play', lights[strand_id][playing[strand_id]]);
+		var d = new Date;
+		playing_started[strand_id] = d.getTime();						
+	}
+		
+	socket.on('disconnect', function (data) {		
+		console.log("disconnect");	
+		for (var i=0; i< lights[strand_id].length; i++) {
+			if (lights[strand_id][i] == id) {
+				lights[strand_id].splice(i, 1);
+				io.sockets.emit('alert_count', {'strand_id': strand_id, 'count': lights[strand_id].length});
+				if (i == playing[strand_id]) {
+					if (playing[strand_id] <= lights[strand_id].length - 1) {
+						io.sockets.emit('play', lights[strand_id][playing[strand_id]]);			
+						var d = new Date;
+						playing_started[strand_id] = d.getTime();
+					} else {
+						playing[strand_id] = 0;
+						io.sockets.emit('play', lights[strand_id][playing[strand_id]]);			
+						var d = new Date;
+						playing_started[strand_id] = d.getTime();						
+					}
+				}
+				
+				break;
+			}
+		}
+
+  });
+
+	socket.on('done', function(socket){
+		
+		if (playing[strand_id] < lights[strand_id].length - 1) {
+			playing[strand_id]++;
+			io.sockets.emit('play', lights[strand_id][playing[strand_id]]);	
+			var d = new Date;
+			playing_started[strand_id] = d.getTime();							
+		} else {
+			playing[strand_id] = 0;
+			io.sockets.emit('play', lights[strand_id][playing[strand_id]]);			
+			var d = new Date;
+			playing_started[strand_id] = d.getTime();			
+		}
+	});
+	
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ public/js/main.js
@@ -0,0 +1,136 @@
+var lights = function() {
+	return {
+		
+		socket: null,
+		my_id: null,
+		strand_id: null,
+		
+		init : function () {
+			
+			var width = $(window).width();
+
+			lights.windowResize();
+			
+			$(window).resize(function(){ lights.windowResize(); });
+
+			$('a#close').click(function(){$(this).parent().remove(); return false;});
+
+			lights.socket = io.connect('http://dev.deeplocal.com:8100');					
+
+			lights.socket.on('connect', function () {				
+				//console.log('CONNECTED!');																								
+			});			
+			
+			lights.socket.on('set_id', function (id) {				
+				lights.my_id = id;
+				//console.log('setting ID: ' + id);																								
+			});			
+			
+			lights.socket.on('set_strand_id', function (id) {				
+				lights.strand_id = id;
+				//console.log('setting strand ID: ' + id);																								
+			});		
+			
+			lights.socket.on('alert_count', function (vals) {				
+				if (vals.strand_id == lights.strand_id) {																													
+					if (vals.count != 1) {
+						$('#count').text(vals.count + ' connections');
+					} else {
+						$('#count').text(vals.count + ' connection');
+					}
+				}
+			});
+			
+			lights.socket.on('play', function(id){
+				if (lights.my_id == id) {
+					$('.lights li').removeClass('on');
+					$('.lights li:first').addClass('on');				
+					var time = setTimeout("lights.nextLight()", 150);					
+				}
+			
+			});
+			
+			
+			$('ul.lights div').live('click', function(){
+				var colors = new Array('real', 'red', 'green', 'yellow', 'blue', 'real');
+				for(var i = 0; i<colors.length-1; i++) {
+					if ($(this).hasClass(colors[i])) {
+						$(this).removeClass(colors[i]).addClass(colors[i+1]);
+						break;
+					}
+				}
+			});
+			
+			
+			$('a#more').click(function(){
+				if ($(this).text() == 'more') { 	
+					$('#header').slideDown(100);
+					$(this).text('hide');
+				} else {
+					$('#header').slideUp(100);
+					$(this).text('more');					
+				}
+				return false;
+			});
+		},
+		
+		nextLight : function() {
+			$('.lights li.on').removeClass('on').next('li').addClass('on');						
+			if ($('.lights li.on').length == 0){ 
+				// do nothing
+				lights.socket.emit('done');
+			} else if ($('.lights li.on').nextAll('li').length == 1) { 
+//				setTimeout("lights.socket.emit('done')", 100);
+				setTimeout("lights.nextLight()", 150);
+			} else {
+				setTimeout("lights.nextLight()", 150);				
+			}
+		},
+		
+		initIndex: function() {
+			$('#join').click(function(){
+				window.location = '/lights/' + $('#value').val();
+			});
+		},
+		
+		windowResize: function() {
+			var width = $(window).width();
+			if (width >= ($('.lights li').length * 120) + 120)  { 
+				while (width >= ($('.lights li').length * 120) + 120)  {
+
+					var degrees = 0;
+					degrees = Math.floor(Math.random() * 20);
+					if (degrees % 2 == 0) {
+						degrees = degrees * -1;
+					}	
+					
+					if (degrees % 7 == 0) {
+						degrees = degrees + 180;
+					}					
+
+					var color = 0;
+					var color_defs = new Array("real", "real", "real", "real", "real", "real");
+					x = (Math.floor(Math.random() * 6));
+					color = color_defs[x];
+
+					var offset = 0;
+
+					x = (Math.floor(Math.random() * 20));
+					offset = -1 * x;
+					
+					if (degrees >= 160 ) {
+						offset = offset - 90;
+					}
+
+					var li = '<li style="position: relative; top:'+ offset +'px;"><div class="'+color+'" style="-webkit-transform: rotate('+ degrees +'deg);-moz-transform: rotate('+ degrees +'deg);"></div></li>';
+					$('.lights').append(li);
+				}
+			} else {
+				while (width < $('.lights li').length * 120) {
+					$('.lights li:last').remove();
+				}
+			}
+		}
+		
+	};
+}();
